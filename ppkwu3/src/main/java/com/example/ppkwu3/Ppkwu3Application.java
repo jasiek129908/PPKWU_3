778fde50cb0f698a63dd000c5990c2de10f4a22f
Jan Oleksik <222529@edu.p.lodz.pl>
2021-11-09 12:22:25 +0100

    Init project

---
+++ ppkwu3/src/main/java/com/example/ppkwu3/Ppkwu3Application.java
@@ -0,0 +1,13 @@
+package com.example.ppkwu3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ppkwu3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ppkwu3Application.class, args);
+    }
+
+}
---
+++ ppkwu3/src/test/java/com/example/ppkwu3/Ppkwu3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ppkwu3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ppkwu3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
