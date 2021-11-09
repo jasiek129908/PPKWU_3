4c6536d4dbbd03135ce7e3df4b874710fb88c604
Jan Oleksik <222529@edu.p.lodz.pl>
2021-11-09 13:01:21 +0100

    added implemenation of get response in json

--- ppkwu3/src/main/java/com/example/ppkwu3/Controller.java
+++ ppkwu3/src/main/java/com/example/ppkwu3/Controller.java
@@ -1,7 +1,10 @@
 package com.example.ppkwu3;
 
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -15,17 +18,21 @@ import java.util.function.Function;
 public class Controller {
 
     @GetMapping("/json")
-    public String getResponseAsJson() {
-        Mono<ResponseApi> text = WebClient.create("http://localhost:8080")
+    public ResponseEntity<ResponseApi> getResponseAsJson(@RequestParam("text") String textToProcess) {
+        ResponseApi responseApi = callExternalStringUtilityApi("http://localhost:8080", textToProcess);
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseApi);
+    }
+
+
+    private ResponseApi callExternalStringUtilityApi(String path, String textToProcess) {
+        Mono<ResponseApi> text = WebClient.create(path)
                 .get()
                 .uri(uriBuilder -> uriBuilder.path("/string")
-                        .queryParam("text", "has  */321AX")
+                        .queryParam("text", textToProcess)
                         .build())
                 .retrieve()
                 .bodyToMono(ResponseApi.class);
-        System.out.println(text.block());
-
-        return "halo";
+        return text.block();
     }
-
 }
