98fbcb43d3eb8e36e946b05716e9c277efe67d18
Jan Oleksik <222529@edu.p.lodz.pl>
2021-11-09 13:24:07 +0100

    added implemenation of get response in xml

--- ppkwu3/src/main/java/com/example/ppkwu3/Controller.java
+++ ppkwu3/src/main/java/com/example/ppkwu3/Controller.java
@@ -1,18 +1,14 @@
 package com.example.ppkwu3;
 
-import org.springframework.http.HttpMethod;
+import org.json.JSONObject;
+import org.json.XML;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.UriBuilder;
 import reactor.core.publisher.Mono;
-
-import java.net.URI;
-import java.util.function.Function;
 
 @RestController
 public class Controller {
@@ -24,6 +20,21 @@ public class Controller {
         return ResponseEntity.status(HttpStatus.OK).body(responseApi);
     }
 
+    @GetMapping(value = "/xml", produces = {"application/xml", "text/xml"})
+    public String getResponseAsXml(@RequestParam("text") String textToProcess) {
+        ResponseApi responseApi = callExternalStringUtilityApi("http://localhost:8080", textToProcess);
+
+        String xmlResponse = createXmlResponse(responseApi);
+        return xmlResponse;
+    }
+
+    private String createXmlResponse(ResponseApi responseApi) {
+        JSONObject jsonObject = new JSONObject(responseApi);
+        StringBuilder res = new StringBuilder("<response>");
+        res.append(XML.toString(jsonObject));
+        res.append("</response>");
+        return res.toString();
+    }
 
     private ResponseApi callExternalStringUtilityApi(String path, String textToProcess) {
         Mono<ResponseApi> text = WebClient.create(path)
